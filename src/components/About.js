611ebc077c29272848d4f3dfe9d6c31b68c971f6
jonas-hirsch <jonasjuanciervos@gmail.com>
2021-06-15 14:53:12 +0200

    about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,31 @@
 import React from 'react'
 import './About.css'
 
+
 const About = () => {
 	return (
-		<div>
+		<div className='aboutpage'>
 			<h1>About</h1>
+			<div className='team'>
+			{/* Minja*/}  
+			<div className='staff'>
+			<h2>Minja</h2>
+				<image></image>
+				<p>Position: </p>
+			</div>
+			{/* Zina*/}
+			<div className='staff'>
+			<h2>Zina</h2>
+				<image></image>
+				<p>Position: Content</p>
+			</div>
+			{/* Jonas*/}
+			<div className='staff'>
+				<h2>Jonas</h2>
+				<image></image>
+				<p>Position: Content and Coffeemake</p>
+			</div>
+			</div>
 		</div>
 	)
 }
