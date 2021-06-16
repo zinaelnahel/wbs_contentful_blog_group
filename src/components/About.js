0d6eb466c3b2e9924afa69b5788217e987703ece
jonas-hirsch <jonasjuanciervos@gmail.com>
2021-06-16 13:57:19 +0200

    thank you for you purchase

--- src/components/About.js
+++ src/components/About.js
@@ -9,19 +9,19 @@ const About = () => {
         <div className="staff">
           <h3>Minja</h3>
           <img className='profilpicture' src="./contact.jpeg" alt="bla" ></img>
-          <p>Position: </p>
+          
         </div>
         {/* Zina*/}
         <div className="staff">
           <h3>Zina</h3>
           <img className='profilpicture' src='./zinapicture.jpeg' alt='zinas picture'></img>
-          <p>Position: Content</p>
+          
         </div>
         {/* Jonas*/}
         <div className="staff">
           <h3>Jonas</h3>
           <img className='profilpicture' src='./jonas.png' alt='jonas profil'></img>
-          <p>Position: Content and Coffeemake</p>
+          
         </div>
       </div>
     </div>
